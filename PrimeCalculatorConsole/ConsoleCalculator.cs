4d5b974eca79aa4f6f0ae5049ca576d7560110d0
Jesse Stromwick <jstromwick@gmail.com>
2016-06-23 15:30:00 -0700

    Making ConsoleCalcluator more efficient with its IO. Updating ReadMe to reflect that solution contains both a WPF and Console frontend

--- PrimeCalculatorConsole/ConsoleCalculator.cs
+++ PrimeCalculatorConsole/ConsoleCalculator.cs
@@ -5,6 +5,8 @@ namespace PrimeCalculatorConsole
 {
     public class ConsoleCalculator : HighestPrimeCalculator
     {
+        private int _primesSoFar;
+
         protected override void OnEveryThousandMilliseconds(long millisecondsElapsed)
         {
             Console.WriteLine("Seconds passed: {0}", (int) millisecondsElapsed/1000);
@@ -12,7 +14,12 @@ namespace PrimeCalculatorConsole
 
         protected override void OnLargestPrimeFound(long largestPrime)
         {
-            Console.WriteLine($"Largest Prime Found So Far: {largestPrime}");
+            _primesSoFar++;
+            if (_primesSoFar == 10000)
+            {
+                _primesSoFar = 0;
+                Console.WriteLine($"Largest Prime Found So Far: {largestPrime}");
+            }
         }
 
         protected override void OnFinishedCalculation(long largestPrime, uint timelimitInSeconds)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PrimeCalcuator
-WPF App that calculates the highest prime number it can in 60 seconds
+A library that calculates the highest prime number it can in 60 seconds.
+
+Solution includes projects to use the library either as a WPF or Console App
